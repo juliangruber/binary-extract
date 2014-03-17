2f77e2423631efada554b43f20ef98d20feece66
Julian Gruber <julian@juliangruber.com>
2014-03-17 11:29:10 +0100

    more robustness

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 module.exports = extract;
 
 var comma = ','.charCodeAt(0);
+var cbrace = '}'.charCodeAt(0);
+var colon = ':'.charCodeAt(0);
 
 function extract(buf, key){
+  var isKey = true;
+
   for (var i = 0; i < buf.length; i++) {
+    if (buf[i] == colon) isKey = false;
+    if (buf[i] == comma) isKey = true;
+    if (!isKey) continue;
+
     var match = true;
     for (var j = 0; j < key.length; j++) {
       if (buf[i + j] != key.charCodeAt(j)) {
@@ -17,7 +25,7 @@ function extract(buf, key){
     var start = i + key.length + 2;
     var end;
     for (var j = start; j < buf.length; j++) {
-      if (buf[j] == comma) {
+      if (buf[j] == comma || buf[j] == cbrace) {
         end = j;
         break;
       }
--- test.js
+++ test.js
@@ -3,18 +3,21 @@ var assert = require('assert');
 var equal = assert.equal;
 var extract = require('./');
 
-var obj = {
-  foo: 'bar',
-  bar: 3,
-  baz: {
-    beep: 'boop'
-  }
-}
-var buf = Buffer(JSON.stringify(obj));
-
 describe('extract(buf, key)', function(){
   it('should extract a value', function(){
+    var buf = toBuf({ foo: 'bar' });
     equal(extract(buf, 'foo'), 'bar');
+  })
+  it('should end on ,', function(){
+    var buf = toBuf({ foo: 'bar', bar: 'baz' });
+    equal(extract(buf, 'foo'), 'bar');
+  })
+  it('should ignore values when looking for keys', function(){
+    var buf = toBuf({ foo: 'bar', bar: 'baz' });
+    equal(extract(buf, 'bar'), 'baz');
   })
 })
 
+function toBuf(obj){
+  return new Buffer(JSON.stringify(obj));
+}
